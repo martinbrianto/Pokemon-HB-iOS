aef8585ac4566e1485d01fb9a6205b88ae8523aa
Gratianus Martin B <martin.gratianus@hotmail.com>
2023-11-07 23:38:18 +0700

    Initial Commit

---
+++ Pokemon HB iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon HB iOS
+//
+//  Created by Martin Brianto on 07/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
